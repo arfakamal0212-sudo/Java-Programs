9bc0623577a97987b903bb2441e8f648240b6ad9
Arfa <arfakamal0212@gmail.com>
2026-06-16 02:59:35 +0530

    Added Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Factorial
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,fact=1;
+        System.out.print("Enter Number: ");
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.print("Factorial of Number: "+ fact);
+    }
+    
+}
